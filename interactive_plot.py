fe16977b68677a457ce7a4819b420f391b0c124d
neurotronk <ajvalpe@gmail.com>
2022-12-27 11:09:36 +0100

    [st]

---
+++ interactive_plot.py
@@ -0,0 +1,26 @@
+import time
+import pylab as pl
+from IPython import display
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Read the data
+
+vertical_data=r'../vision_ejes/data/gaze_positions_verticales.csv'
+horizontal_data=r'../vision_ejes/data/gaze_positions - copia.csv'
+
+vertical_test=pd.read_csv(vertical_data,delimiter=';',decimal=',')
+horizontal_test=pd.read_csv(horizontal_data,delimiter=';',decimal=',')
+
+print(vertical_test.head())
+
+
+for timestamp,_ in enumerate(vertical_test['gaze_normal0_x']):
+    if timestamp%10!=0:
+        continue
+    plt.plot(vertical_test['gaze_normal0_x'][timestamp],vertical_test['gaze_normal0_y'][timestamp],'o',color='black')
+    plt.title('time= {}'.format(timestamp))
+    time.sleep(0.005)
+    plt.show()
